e7e19aa72bcf75a852c6dfc06710fb762a3e06af
Miqueias <miqueias.caetano@doctum.edu.br>
2021-03-09 16:23:57 -0300

    Ok

--- platforms/android/app/build/generated/source/buildConfig/release/br/com/innovatesof/quizbiblico/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/br/com/innovatesof/quizbiblico/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "br.com.innovatesof.quizbiblico";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 10002;
-  public static final String VERSION_NAME = "1.0.2";
+  public static final int VERSION_CODE = 10003;
+  public static final String VERSION_NAME = "1.0.3";
 }
